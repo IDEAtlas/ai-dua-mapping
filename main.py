74e02ac10043592d61053e7fd24c489cb79e141f
rvmaretto <rvmaretto@gmail.com>
2025-09-03 14:16:45 +0000

    Changing path concatenation.

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def print_info(name, data):
 batch = args.batch if args.batch else config.BATCH_SIZE
 epochs = args.epochs if args.epochs else config.EPOCHS
 city = args.city
-dir = config.DATA_PATH + city + '/'
+dir = os.path.join(config.DATA_PATH, city)
 
 
 inputs = config.DATASET
@@ -65,15 +65,15 @@ if args.stage == "train":
     print('Training model on', city.capitalize(), 'dataset')
     print('Input: ', inputs)
 
-    train_images = [dl.load_data(dir + 'train', h, w, input) for input in inputs]
-    val_images = [dl.load_data(dir + 'val', h, w, input) for input in inputs]
-    
+    train_images = [dl.load_data(os.path.join(dir, 'train'), h, w, input) for input in inputs]
+    val_images = [dl.load_data(os.path.join(dir, 'val'), h, w, input) for input in inputs]
+
     if len(inputs) == 1:
         train_images = train_images[0]
         val_images = val_images[0]
 
-    train_label = dl.load_data(dir + 'train', h, w, 'RF', config.N_CLASSES)
-    val_label = dl.load_data(dir + 'val', h, w, 'RF', config.N_CLASSES)
+    train_label = dl.load_data(os.path.join(dir, 'train'), h, w, 'RF', config.N_CLASSES)
+    val_label = dl.load_data(os.path.join(dir, 'val'), h, w, 'RF', config.N_CLASSES)
 
     print_info("Train Images", train_images)
     print_info("Validation Images", val_images)
@@ -124,10 +124,20 @@ if args.stage == "train":
     #     }
     # )
 
+    # Check if CHECKPOINT_PATH exists and creates it if it doesn't
+    if not os.path.exists(config.CHECKPOINT_PATH):
+        os.makedirs(config.CHECKPOINT_PATH)
+
+    # Remove old checkpoint files
     for i in os.listdir(config.CHECKPOINT_PATH):
         if i.endswith('.h5'):
             os.remove(os.path.join(config.CHECKPOINT_PATH, i))
 
+    # Check if LOG_PATH exists and creates it if it doesn't
+    if not os.path.exists(config.LOG_PATH):
+        os.makedirs(config.LOG_PATH)
+
+    # Remove old log files
     for j in os.listdir(config.LOG_PATH):
         if j.endswith('.csv'):
             os.remove(os.path.join(config.LOG_PATH, j))
@@ -201,13 +211,19 @@ elif args.stage == "test":
     weight = args.weight if args.weight else (f'./{config.CHECKPOINT_PATH}/{city}.{inputs_str}.{model.name}.weights.h5')
     # weight = './checkpoint/best/buenos_aires_s2_morph_mbcnn.weights.h5'
     print(f'Model weight: {weight}')
+
+    # Raise an exception if the weights file does not exist
+    if not os.path.exists(weight):
+        raise FileNotFoundError(f"Weight file not found: {weight}\n"
+                            "Check --weight, config.CHECKPOINT_PATH, inputs in config.json, or run training to create this file.")
+    
     model.load_weights(weight)
 
-    test_images = [dl.load_data(dir + 'test', h, w, input) for input in inputs]
+    test_images = [dl.load_data(os.path.join(dir, 'train'), h, w, input) for input in inputs]
     if len(inputs) == 1:
         test_images = test_images[0]
 
-    test_label = dl.load_data(dir + 'test', h, w, 'RF', config.N_CLASSES)
+    test_label = dl.load_data(os.path.join(dir, 'test'), h, w, 'RF', config.N_CLASSES)
 
     print_info("Test Images", test_images)
     print_info("Test Label", test_label)
@@ -230,6 +246,12 @@ elif args.stage == "infer":
 
     model = models.select_model(args.model, config)
     weight = args.weight if args.weight else (f'./{config.CHECKPOINT_PATH}/{city}.{inputs_str}.{model.name}.weights.h5')
+    
+    # Raise an exception if the weights file does not exist
+    if not os.path.exists(weight):
+        raise FileNotFoundError(f"Weight file not found: {weight}\n"
+                            "Check --weight, config.CHECKPOINT_PATH, inputs in config.json, or run training to create this file.")
+    
     model.load_weights(weight)
     aoi_path = f"{config.AOI}/{city}_aoi.geojson"
     save_path = f'{config.PREDICTION_PATH}/{city}_{inputs_str}_{model.name}_{year}.tif'
